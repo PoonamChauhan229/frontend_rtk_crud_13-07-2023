1af563528595ac37110d9fc9eabbd36e3f805a38
Poonam229 <poonam.chauhan229@gmail.com>
2023-10-06 15:03:55 +0530

    link changed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 // export const taskList='https://647588cde607ba4797dbfe60.mockapi.io/task'
 
-export const taskListUrl=`http://localhost:5000`
+// export const taskListUrl=`http://localhost:5000`
+
+export const taskListUrl=`https://fine-cyan-jackrabbit-ring.cyclic.app`
